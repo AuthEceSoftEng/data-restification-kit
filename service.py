1cf114eb578edaa00aa481c2841dc9e44426b6b3
Michail Papamichail <papa.mich@hotmail.com>
2019-02-24 16:24:57 +0200

    Updated main service

--- service.py
+++ service.py
@@ -8,73 +8,175 @@ from flask import Flask, Response, request
 from utilities.datasetsHandler import datasetsHandler
 from utilities.dataImporter import dataImporter
 
+app = Flask(__name__)
+global processes_info
+processes_info = {}
+processes_info["importer"] = None
+processes_info["retriever"] = None
+processes_info["importer_available"] = True
+
 def kill(proc_pid):
     process = psutil.Process(proc_pid)
     for proc in process.children(recursive=True):
         proc.kill()
     process.kill()
 
-app = Flask(__name__)
-
-@app.route('/api/startImporter')
-def start():
+def startImporter():
+     
+    if(processes_info["importer"] is None):
+        try:
+            proc = subprocess.Popen(['C:\Python34\python.exe', 'ImportService' + os.sep + 'ImportService.py'], shell=True)
+            processes_info["importer"] = proc.pid
+             
+            return True
+        except Exception as e:
+            return False
     
-    global proc
-    try:
-        proc = subprocess.Popen(['C:\Python34\python.exe', 'ImportService' + os.sep + 'ImportService.py'], shell=True)
-        return Response(json.dumps({"message": "Service started successfully"}, indent=3), mimetype="application/json")
-    except Exception as e:
-        return Response(json.dumps({"message": "Service could not start", "exception": e}, indent=3), mimetype="application/json")
+    return True
+
+def stopImporter():
+    
+    if(processes_info["importer"] is not None):
+        kill(processes_info["importer"])
+    processes_info["importer"] = None
+ 
+    return True
+
+def startRetriever():
+     
+    if(processes_info["retriever"] is None):
+        try:
+            proc = subprocess.Popen(['C:\Python34\python.exe', 'RetrieveService' + os.sep + 'RetrieveService.py'], shell=True)
+            processes_info["retriever"] = proc.pid
+             
+            return True
+        except Exception as e:
+            return False
+    
+    return True
+
+def stopRetriever():
+    
+    if(processes_info["retriever"] is not None):
+        kill(processes_info["retriever"])
+    processes_info["retriever"] = None
+ 
+    return True
+
+# @app.route('/api/startImporter')
+# def startImporter():
+#     
+#     try:
+#         proc = subprocess.Popen(['C:\Python34\python.exe', 'ImportService' + os.sep + 'ImportService.py'], shell=True)
+#         processes_info["importer"] = proc.pid
+#         
+#         return Response(json.dumps({"message": "Service started successfully"}, indent=3), mimetype="application/json")
+#     except Exception as e:
+#         return Response(json.dumps({"message": "Service could not start", "exception": e}, indent=3), mimetype="application/json")
 #     try:
 #         proc.wait(timeout=1000)
 #     except subprocess.TimeoutExpired:
 #         kill(proc.pid)
 #         return Response(json.dumps({"message": "Service Stopped"}, indent=3), mimetype="application/json")
 
-@app.route('/api/checkImporter')
+# @app.route('/api/stopImporter')
+# def stopImporter():
+#     kill(processes_info["importer"])
+#     processes_info["importer"] = None
+# 
+#     return Response(json.dumps({"message": "Service Stopped"}, indent=3), mimetype="application/json")
+
+
+@app.route('/api/check')
 def check():
     
-    if(('proc' in globals()) and (proc is not None)):
-        return Response(json.dumps({"message": "The import service is running. pid: " + str(proc.pid)}, indent=3), mimetype="application/json")
+    info = {}
+    if(processes_info["importer"] is not None):
+        info["importer"] = "The import service is running. pid: " + str(processes_info["importer"])
     else:
-        return Response(json.dumps({"message": "The import service is not running"}, indent=3), mimetype="application/json")
+        info["importer"] = "The import service is not running"
+    if(processes_info["importer_available"]):
+        info["importer_availability"] = "The import service is available"
+    else:
+        info["importer_availability"] = "The import service is running"
+    if(processes_info["retriever"] is not None):
+        info["retriever"] = "The retrieve service is running. pid: " + str(processes_info["retriever"])
+    else:
+        info["retriever"] = "The retrieve service is not running"
+        
+    return Response(json.dumps(info, indent=3), mimetype="application/json")
 
-
-@app.route('/api/stopImporter')
-def stop():
-    kill(proc.pid)
-    global proc
-    proc = None
+@app.route('/api/validateDataset', methods = ["POST"])
+def validateDataset():
     
-    return Response(json.dumps({"message": "Service Stopped"}, indent=3), mimetype="application/json")
+    if(("datasetName" in request.args) and ("separator" in request.args) and ("dataFile" in request.files)):
+        dataset_file = request.files["dataFile"]
+        
+        dH = datasetsHandler(request.args["datasetName"], dataset_file)
+        
+        val = dH.validate('all', request.args["separator"])
+        if(val == True):
+            info = {"schema": dH.schema}
+            with open('schemas/' + request.args["datasetName"] + '.schema', 'w') as schema_file:
+                json.dump(dH.schema, schema_file, indent=3, sort_keys=True)
+            res = json.dumps(info, indent=3)
+             
+            return Response(res, status = 200, mimetype="application/json")
+        else:
+            res = json.dumps(val, indent=3)
+             
+            return Response(res, status = 400, mimetype="application/json")
+    
+    else:
+        res = json.dumps({"error": "Missing parameters. You should provide datasetName, separator and dataFile"}, indent=3)
+        return Response(res, status = 400, mimetype="application/json")
+
+@app.route('/api/importDataset', methods = ["POST"])
+def importDataset():
+    
+    if(processes_info["importer_available"] == True):
+        processes_info["importer_available"] = False
+        if(("datasetName" in request.args) and ("separator" in request.args) and ("dataFile" in request.files)):
+            dataset_file = request.files["dataFile"]
+            if(startImporter()):
+                dH = datasetsHandler(request.args["datasetName"], dataset_file)
+                dI = dataImporter(dH)
+                dI.import_data('http://localhost:5050/api/v1/')
+                if(stopImporter()):
+                    res = json.dumps({"error": "Import successful"}, indent=3)
+                    processes_info["importer_available"] = True
+                    return Response(res, status = 201, mimetype="application/json")
+                else:
+                    res = json.dumps({"error": "Importer stopping procedure failed"}, indent=3)
+                    processes_info["importer_available"] = True
+                    return Response(res, status = 400, mimetype="application/json")
+            else:
+                res = json.dumps({"error": "Importer starting procedure failed"}, indent=3)
+                processes_info["importer_available"] = True
+                return Response(res, status = 400, mimetype="application/json")
+        else:
+            res = json.dumps({"error": "Missing parameters. You should provide datasetName, separator and dataFile"}, indent=3)
+            processes_info["importer_available"] = True
+            return Response(res, status = 400, mimetype="application/json")
+    else:
+        res = json.dumps({"error": "Another import currently in progress. Please try again"}, indent=3)
+        return Response(res, status = 400, mimetype="application/json")
+
+@app.route('/api/restartRetriever')
+def restartRetriever():
+    
+    stopRetriever()
+    if(startRetriever()):
+        res = json.dumps({"message": "Retriever restarted successfully"}, indent=3)
+        return Response(res, status = 200, mimetype="application/json")
+    else:
+        res = json.dumps({"message": "Retriever restart failed"}, indent=3)
+        return Response(res, status = 400, mimetype="application/json")
 
 @app.route('/api/getSchemaTypes')
 def getSchemaTypes():
     
     return Response(json.dumps({"types": [val for val in data['label'].values] }, indent=3), mimetype="application/json")
-
-@app.route('/api/registerDataset', methods = ["POST"])
-def validateDataset():
-    
-    print(request.files)
-    dataset_file = request.files["dataFile"]
-    print(type(request.files["dataFile"]))
-    print(dataset_file.filename)
-    #r = pd.read_csv(dataset_file)
-    dH = datasetsHandler('michalis32', dataset_file)
-    
-    val = dH.validate('all', ',')
-    if(val == True):
-        info = {"schema": dH.schema}
-        with open('schemas/' + dH.dataset_name + '.schema', 'w') as schema_file:
-            json.dump(dH.schema, schema_file, indent=3, sort_keys=True)
-        res = json.dumps(info, indent=3)
-        
-        return Response(res, status = 200, mimetype="application/json")
-    else:
-        res = json.dumps(val, indent=3)
-        
-        return Response(res, status = 400, mimetype="application/json")
     
 @app.route('/api/getSchemaProperties')
 def getSchemaProperties():
@@ -98,7 +200,7 @@ def getSchemaProperties():
     else:
         res = json.dumps({"error": "The URL parameter type must be specified"}, indent=3)
         return Response(res, status = 400, mimetype="application/json")
-    
+
 if __name__ == '__main__':
     # Load schema types
     data = pd.read_csv(os.path.join('utilities', 'schema_data', 'schema-types.csv'))
