b6748fc8b69e202e2f1b7befa3f17fbde4ff0a9d
tasos <tasoskakour@gmail.com>
2019-02-27 12:19:08 +0200

    Added cors also to retriever and importer

--- RetrieveService/RetrieveService.py
+++ RetrieveService/RetrieveService.py
@@ -6,6 +6,7 @@ from eve import Eve
 from eve.auth import BasicAuth
 from properties import retrieve_service_auth, retrieve_service_username, retrieve_service_password, \
                        retrieve_service_host, retrieve_service_port
+from flask_cors import CORS, cross_origin
 
 class MyBasicAuth(BasicAuth):
     def check_auth(self, username, password, allowed_roles, resource, method):
@@ -15,7 +16,7 @@ if(retrieve_service_auth):
     app = Eve(auth = MyBasicAuth, settings="RetrieveSettings.py")
 else:
     app = Eve(settings="RetrieveSettings.py")
-
+CORS(app, support_credentials = True)
 
 if __name__ == '__main__':
     app.run(host = retrieve_service_host, port = retrieve_service_port)
